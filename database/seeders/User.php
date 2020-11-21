7b8694f62dbaaeecb55d5b1a3d8721cbf6656a3e
alfin1998 <alfinyuki@gmail.com>
2020-11-21 18:21:28 +0700

    Update seeder + Faker

---
+++ database/seeders/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+
+class User extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $faker = Faker::create('id_ID');
+
+        for($i = 1; $i <= 10; $i++){
+        DB::table('user')->insert([
+        	'nama' => $faker->name,
+            'Password' => $faker->password,
+            'alamat' => $faker->address
+        	
+        ]);
+        }
+    
+    }
+}
